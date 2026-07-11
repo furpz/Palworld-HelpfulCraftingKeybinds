91c95b0c244bcb2eb07b4cad4d80e779a109b91b
furpz <196845995+furpz@users.noreply.github.com>
2026-07-11 10:01:24 -0500

    implemented typing check and shift modifier

--- Scripts/main.lua
+++ Scripts/main.lua
@@ -2,8 +2,25 @@ local ueHelpers = require("UEHelpers")
 
 local lastSelectedAmount
 
-local function SplitAmount(commonSelectNum, denominator, instantCraft)
+local function IsPlayerTyping(workspace)
+    if workspace and workspace:IsValid() then
+        local searchBar = workspace.PalEditableTextBox_Search
+        if searchBar and searchBar:IsValid() then
+            if searchBar:HasKeyboardFocus() then
+                return true
+            end
+        end
+    end
+
+    return false
+end
+
+local function SplitAmount(workspace, denominator, instantCraft)
+    if IsPlayerTyping(workspace) then return end
+
     instantCraft = instantCraft or false
+
+    local commonSelectNum = workspace.WBP_IngameCommonSelectNum --please sanity check this
 
     if denominator <= 0 then return end
     if commonSelectNum and commonSelectNum:IsValid() then
@@ -14,7 +31,7 @@ local function SplitAmount(commonSelectNum, denominator, instantCraft)
         commonSelectNum:SetNum(amountToSelect, 1, true)
 
         if instantCraft then
-            
+            workspace:StartProduce()
         end
     end
 end
@@ -28,58 +45,39 @@ local keyToDenominator = { -- so i dont have to write registerkeybind like 10 ti
     ["SIX"] = 6,
 }
 
+local function InitializeForWorkspace(workspace)
+    if not string.find(workspace:GetFullName(), "/Engine/Transient") then return end
+
+    for keyName, denominator in pairs(keyToDenominator) do
+        local targetKey = Key[keyName]
+
+        RegisterKeyBind(targetKey, function()
+            SplitAmount(workspace, denominator)
+        end)
+
+        RegisterKeyBind(targetKey, {ModifierKey.SHIFT}, function()
+            SplitAmount(workspace, denominator, true)
+        end)
+    end
+end
+
 local function InitializeForHotReload()
-    local sliders = FindAllOf("WBP_IngameMenu_WorkSpace_Slider_C")
-
-    for i, slider in pairs(sliders) do 
-        local commonSelectNum = slider:GetOuter():GetOuter()
-        if string.find(commonSelectNum:GetFullName(), "/Engine/Transient") then
-
-            for keyName, denominator in pairs(keyToDenominator) do
-                local targetKey = Key[keyName]
-
-                RegisterKeyBind(targetKey, function()
-                    SplitAmount(commonSelectNum, denominator)
-                end)
-
-                RegisterKeyBind(targetKey, function()
-                    SplitAmount(commonSelectNum, denominator, true)
-                end)
-            end
-
-
-        end
-
+    local workspaces = FindAllOf("WBP_IngameMenu_WorkSpace_C") --really could just do findfirstof but wtv
+    for i, workspace in pairs(workspaces) do
+        InitializeForWorkspace(workspace)
     end
 end
 
 InitializeForHotReload()
 
--- RegisterHook("/Script/Engine.PlayerController:ServerAcknowledgePossession", function(self)
---     NotifyOnNewObject("/Game/Pal/Blueprint/UI/UserInterface/IngameMenu/WBP_IngameMenu_WorkSpace_Slider.WBP_IngameMenu_WorkSpace_Slider_C", function(slider)
---         -- print(slider:GetFullName())
 
---         local commonSelectNum = slider:GetOuter():GetOuter()
---         if not string.find(commonSelectNum:GetFullName(), "/Engine/Transient") then return end
-
---         RegisterKeyBind(Key.ONE, function()
---             local max = commonSelectNum["Max Num"]
---             print(commonSelectNum:GetFullName())
-            
-            
---             ExecuteInGameThread(function()
---                 SplitAmount(commonSelectNum, 3)
---             end)
---         end)
-
---         print(commonSelectNum:GetFullName() + "/OnVisibilityChangedEvent")
---         RegisterHook(commonSelectNum:GetFullName() + "/OnVisibilityChangedEvent", function(visibility)
---             print("visibiltiy changed")
---         end)
---     end)
--- end)
-
-
+RegisterHook("/Script/Engine.PlayerController:ServerAcknowledgePossession", function(self)
+    --notifying on new WBP_IngameMenu_WorkSpace_Slider_C results in two occurances, which isn't the case it seems for WBP_IngameMenu_WorkSpace_C
+    --checking if path contains /Engine/Transient just in case, but it prob doesn't matter
+    NotifyOnNewObject("/Game/Pal/Blueprint/UI/UserInterface/IngameMenu/WBP_IngameMenu_WorkSpace.WBP_IngameMenu_WorkSpace_C", function(workspace)
+        InitializeForWorkspace(workspace)
+    end)
+end)
 
 
 print("[QuickCraftSplit] MOD LOADED")
