d43fa1baad9d698f9327c275f4a01b59945eb71e
furpz <196845995+furpz@users.noreply.github.com>
2026-07-11 13:23:32 -0500

    made activeworkbench be refreshed each time its needed, unsure if this helps, crash when loading world for second time

--- Scripts/main.lua
+++ Scripts/main.lua
@@ -1,6 +1,20 @@
+--[[
+CRASH NOTES
+load in world, use a keybind, leave, load a world, crashes
+narroed down to using workspace:StartProduce() causing crash, as well as commonselectnum:SetNum()
+
+genuinely lost on this
+
+
+]]
+
+
+
 local ueHelpers = require("UEHelpers")
 
 local lastSelectedAmount
+local activeWorkspace
+
 local keyToDenominator = { -- so i dont have to write registerkeybind like 10 times lol
     ["ONE"] = 1,
     ["TWO"] = 2,
@@ -14,29 +28,46 @@ local function mPrint(message)
     print("[QuickCraftSplit] " .. message)
 end
 
-local function IsPlayerTyping(workspace)
+local function RefreshActiveWorkspace()
+    local workspace = FindFirstOf("WBP_IngameMenu_WorkSpace_C")
     if workspace and workspace:IsValid() then
-        local searchBar = workspace.PalEditableTextBox_Search
-        if searchBar and searchBar:IsValid() then
-            if searchBar:HasKeyboardFocus() then
-                return true
-            end
+        activeWorkspace = workspace
+    end
+end
+
+local function WorkspaceValid()
+    RefreshActiveWorkspace()
+
+    local isValid = activeWorkspace and activeWorkspace:IsValid()
+    print(isValid and "workspace is valid" or "workspace isnt valid")
+    return isValid
+end
+
+local function IsPlayerTyping()
+    if not WorkspaceValid() then return end
+
+    local searchBar = activeWorkspace.PalEditableTextBox_Search
+    if searchBar and searchBar:IsValid() then
+        if searchBar:HasKeyboardFocus() then
+            return true
         end
     end
 
     return false
 end
 
-local function StartCraft(workspace)
-    if workspace:IsActivated() then workspace:StartProduce() end
+local function StartCraft()
+    if IsPlayerTyping() then return end
+
+    if activeWorkspace:IsActivated() then activeWorkspace:StartProduce() end
 end
 
-local function SplitAmount(workspace, denominator, instantCraft)
-    if IsPlayerTyping(workspace) then return end
+local function SplitAmount(denominator, instantCraft)
+    if IsPlayerTyping() then return end
 
     instantCraft = instantCraft or false
 
-    local commonSelectNum = workspace.WBP_IngameCommonSelectNum
+    local commonSelectNum = activeWorkspace.WBP_IngameCommonSelectNum
     if not commonSelectNum or not commonSelectNum:IsValid() then mPrint("csn not valid") end
 
     if denominator <= 0 then return end
@@ -48,45 +79,42 @@ local function SplitAmount(workspace, denominator, instantCraft)
         lastSelectedAmount = amountToSelect
 
         if instantCraft then
-            StartCraft(workspace)
+            StartCraft()
         end
     end
 end
 
-local function UseLastSelectedAmount(workspace, instantCraft) --theres prob a way to do this w/ less duplicated code but im lazy so
-    if IsPlayerTyping(workspace) then return end
+local function UseLastSelectedAmount(instantCraft) --theres prob a way to do this w/ less duplicated code but im lazy so
+    if IsPlayerTyping() then return end
     if not lastSelectedAmount then return end
 
     instantCraft = instantCraft or false
 
-    local commonSelectNum = workspace.WBP_IngameCommonSelectNum
+    local commonSelectNum = activeWorkspace.WBP_IngameCommonSelectNum
     if not commonSelectNum or not commonSelectNum:IsValid() then mPrint("csn not valid") end
 
     commonSelectNum:SetNum(lastSelectedAmount, 1, true)
 end
 
-local function InitializeForWorkspace(workspace) --this should only run once
-    if not (workspace and workspace:IsValid()) then mPrint("workspace not valid") return end
-    if not string.find(workspace:GetFullName(), "/Engine/Transient") then mPrint("improper workspace path") return end
-
+local function SetupKeybinds()
     for keyName, denominator in pairs(keyToDenominator) do
         local targetKey = Key[keyName]
 
         RegisterKeyBind(targetKey, function()
-            SplitAmount(workspace, denominator)
+            SplitAmount(denominator)
         end)
 
         RegisterKeyBind(targetKey, {ModifierKey.SHIFT}, function()
-            SplitAmount(workspace, denominator, true)
+            SplitAmount(denominator, true)
         end)
     end
 
     RegisterKeyBind(Key.SPACE, function()
-        StartCraft(workspace)
+        StartCraft()
     end)
 
     RegisterKeyBind(Key.OEM_THREE, function()
-        UseLastSelectedAmount(workspace)
+        UseLastSelectedAmount()
     end)
 end
 
@@ -99,12 +127,21 @@ end
 
 -- InitializeForHotReload()
 
-RegisterHook("/Script/Engine.PlayerController:ServerAcknowledgePossession", function(self)
-    --notifying on new WBP_IngameMenu_WorkSpace_Slider_C results in two occurances, which isn't the case it seems for WBP_IngameMenu_WorkSpace_C
-    --checking if path contains /Engine/Transient just in case, but it prob doesn't matter
-    NotifyOnNewObject("/Game/Pal/Blueprint/UI/UserInterface/IngameMenu/WBP_IngameMenu_WorkSpace.WBP_IngameMenu_WorkSpace_C", function(workspace)
-        InitializeForWorkspace(workspace)
-    end)
-end)
+SetupKeybinds()
+
+-- RegisterHook("/Script/Engine.PlayerController:ServerAcknowledgePossession", function(self)
+--     --notifying on new WBP_IngameMenu_WorkSpace_Slider_C results in two occurances, which isn't the case it seems for WBP_IngameMenu_WorkSpace_C
+--     --checking if path contains /Engine/Transient just in case, but it prob doesn't matter
+--     NotifyOnNewObject("/Game/Pal/Blueprint/UI/UserInterface/IngameMenu/WBP_IngameMenu_WorkSpace.WBP_IngameMenu_WorkSpace_C", function(workspace)
+--         mPrint("notified")
+--         if not (workspace and workspace:IsValid()) then mPrint("workspace not valid") return end
+--         if not string.find(workspace:GetFullName(), "/Engine/Transient") then mPrint("improper workspace path") return end
+
+--         activeWorkspace = workspace
+--         mPrint("active workspace set")
+
+
+--     end)
+-- end)
 
 mPrint("MOD LOADED")
