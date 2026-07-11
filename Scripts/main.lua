48c86fb511df01ffa5628169daa495638c972006
furpz <196845995+furpz@users.noreply.github.com>
2026-07-10 23:56:48 -0500

    made test mod for familiarity

--- Scripts/main.lua
+++ Scripts/main.lua
@@ -1,26 +1,52 @@
----@meta
-require("Types")
+local UEHelpersLoaded, UEHelpers = pcall(
+    require,
+    "UEHelpers"
+)
 
-local UEHelpers = require("UEHelpers")
+local function HookPalSummon()
+    RegisterHook(
+        "/Game/Pal/Blueprint/Component/OtomoHolder/BP_OtomoPalHolderComponent.BP_OtomoPalHolderComponent_C:ActivateOtomo",
+        function(self, slotId)
+            local HolderComponent = self:Get()
+            local PalActor = HolderComponent:TryGetOtomoActorBySlotIndex(slotId:Get())
 
-function DoCraftShit()
-    local workspaceMenu = FindFirstOf("WBP_IngameMenu_WorkSpace_C")
-    local slider = FindFirstOf("WBP_IngameMenu_WorkSpace_Slider_C")
-
-    if workspaceMenu and workspaceMenu:IsValid() then
-        print("workspace menu valid")
-        pcall(function()
-            print(workspaceMenu.CurrentProductAmount)
-            -- workspaceMenu.CurrentProductAmount = 9
-            -- workspaceMenu:StartProduce()
+            -- PalActor:RequestJump()
+            PalActor:SetActorScale3D({ X = 5, Y = 5, Z = 1 })
         end)
-    end
-
-
-    if slider and slider:IsValid() then
-        print("slider valid yo")
-        print(slider.Current)
-    end
 end
 
-RegisterKeyBind(Key.ONE, DoCraftShit)
+-- function DoCraftShit()
+--     local workspaceMenu = FindFirstOf("WBP_IngameMenu_WorkSpace_C")
+--     local slider = FindFirstOf("WBP_IngameMenu_WorkSpace_Slider_C")
+
+--     if workspaceMenu and workspaceMenu:IsValid() then
+--         print("workspace menu valid")
+--         pcall(function()
+--             print(workspaceMenu.CurrentProductAmount)
+--             -- workspaceMenu.CurrentProductAmount = 9
+--             -- workspaceMenu:StartProduce()
+--         end)
+--     end
+
+
+--     if slider and slider:IsValid() then
+--         print("slider valid yo")
+--         print(slider.Current)
+--     end
+-- end
+
+-- RegisterKeyBind(Key.ONE, DoCraftShit)
+
+-- RegisterHook("/Script/Engine.PlayerController:ClientRestart", function(Context)
+--     NotifyOnNewObject(
+--         "/Game/Pal/Blueprint/Component/OtomoHolder/BP_OtomoPalHolderComponent.BP_OtomoPalHolderComponent_C",
+--         function(Component)
+--             HookPalSummon()
+--         end)
+-- end)
+
+
+--for hot reloading in world purposes
+HookPalSummon()
+
+print("[QuickCraftSplit] MOD LOADED")
